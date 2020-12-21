76ee8c303447f96cb748eaa08515fee3da6e6e2e
axeliono <69375446+axeliono@users.noreply.github.com>
2020-12-20 23:10:54 -0600

    database framework and function framework complete, test queries and input

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,12 +1,27 @@
 const inquirer = require("inquirer");
-const { viewDepartmentQuery } = require("./queries");
+const {
+  viewDepartmentQuery,
+  viewEmployeeQuery,
+  viewRoleQuery,
+  addEmployeeQuery,
+  updateEmployeeQuery,
+  addRoleQuery,
+} = require("./queries");
 const connection = require("./connection.js");
 const questions = [
   {
     type: "list",
     name: "starter",
     message: "What would you like to do?",
-    choices: ["view all departments"],
+    choices: [
+      "view all departments",
+      "view all employees",
+      "view all roles",
+      "add new role",
+      "add new employee",
+      "update existing employee",
+      "end session",
+    ],
   },
 ];
 
@@ -25,6 +40,30 @@ class employeeDatabase {
     await inquirer.prompt(questions).then((answer) => {
       if (answer.starter === "view all departments") {
         viewDepartmentQuery();
+      }
+
+      if (answer.starter === "view all employees") {
+        viewEmployeeQuery();
+      }
+
+      if (answer.starter === "view all roles") {
+        viewRoleQuery();
+      }
+
+      if (answer.starter === "add new employee") {
+        addEmployeeQuery();
+      }
+
+      if (answer.starter === "update existing employee") {
+        updateEmployeeQuery();
+      }
+
+      if (answer.starter === "add new role") {
+        addRoleQuery();
+      }
+
+      if (answer.starter === "end session") {
+        connection.end();
       }
     });
   }
